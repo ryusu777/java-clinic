f1d7a2f536f13f6b97bae2e4f8dab4e194927faa
Veronica Yose Ardilla <veronica.ardilla@student.matanauniversity.ac.id>
2022-03-01 15:59:25 +0700

    Queue domain

--- src/main/java/com/clinic/receptionist/domain/Queue.java
+++ src/main/java/com/clinic/receptionist/domain/Queue.java
@@ -1,5 +1,51 @@
 package com.clinic.receptionist.domain;
 
-public class Queue {
-    
+import java.sql.Date;
+import java.sql.Time;
+
+import com.clinic.AbstractEntity;
+public class Queue extends AbstractEntity{
+    private Integer doctorId, patientId;
+    private Time queueTime;
+    private Date queueDate;
+
+    public Queue(Integer id) {
+        super(id);
+    }
+
+    public Queue(){
+        super(null);
+    }
+
+    public Integer getDoctorId(){
+        return doctorId;
+    }
+
+    public void setDoctorId(Integer doctorId){
+        this.doctorId = doctorId;
+    }
+
+    public Integer getPatientId(){
+        return patientId;
+    }
+
+    public void setPatientId(Integer patientId){
+        this.patientId = patientId;
+    }
+
+    public Time getQueueTime(){
+        return queueTime;
+    }
+
+    public void setQueueTime(Time queueTime){
+        this.queueTime = queueTime;
+    }
+
+    public Date getQueueDate(){
+        return queueDate;
+    }
+
+    public void setQueueTime(Date queueDate){
+        this.queueDate = queueDate;
+    }
 }
