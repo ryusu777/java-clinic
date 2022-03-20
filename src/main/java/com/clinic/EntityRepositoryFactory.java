ca759136aa770b0910e9e8b7bf83e923c1de8da7
Jose Ryu Leonesta <ryunamikaze777@gmail.com>
2022-03-20 21:23:47 +0700

    EntityRepositoryFactory.getRepository() doc.

--- src/main/java/com/clinic/EntityRepositoryFactory.java
+++ src/main/java/com/clinic/EntityRepositoryFactory.java
@@ -6,12 +6,17 @@ import java.util.Map;
 public class EntityRepositoryFactory {
     private static Map<Class<?>, AbstractEntityRepository<?>> repositories = new HashMap<>();
 
+    /**
+     * Get a repository object which should extend 
+     * <code>AbstractEntityRepository</code>.
+     * @param theClass example: MedicineRepository.class
+     * @return instance of <code>theClass</code>
+     */
     @SuppressWarnings("unchecked")
     public static <T extends AbstractEntityRepository<?>> T getRepository(Class<T> theClass) {
         if (!repositories.containsKey(theClass))
             try {
                 repositories.put(theClass, theClass.getConstructor().newInstance());
-                System.out.println("Created " + theClass.getName() + " instance");
             } catch (Exception e) {
                 System.out.println(
                         "Exception in EntityRepositoryFactory.getRepository(Class<T> theClass): " + e.toString());
