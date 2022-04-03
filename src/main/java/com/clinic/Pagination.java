2819183624ee17b9011c99d97e784f6675a37064
Jose Ryu Leonesta <ryunamikaze777@gmail.com>
2022-04-03 22:57:27 +0700

    Pagination default constructor.

--- src/main/java/com/clinic/Pagination.java
+++ src/main/java/com/clinic/Pagination.java
@@ -3,9 +3,15 @@ package com.clinic;
 /**
  * Class for pagination data
  * 
+ * The <code>pageNumber</code> starts from 1
+ * 
  * @author Jose Ryu Leonesta <jose.leonesta@student.matanauniversity.ac.id>
  */
 public class Pagination {
+    public Pagination() {
+        recordsPerPage = 10;
+        pageNumber = 1;
+    }
     public Integer recordsPerPage, pageNumber, totalRecords;
     public String sortBy, sortOrder;
 }
