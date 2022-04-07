4f05770d18ed6bab7db30edafd459bcedcf6c58a
Jose Ryu Leonesta <ryunamikaze777@gmail.com>
2022-04-07 18:46:20 +0700

    MedicineController sample.

--- src/main/java/com/clinic/drug/controller/MedicineController.java
+++ src/main/java/com/clinic/drug/controller/MedicineController.java
@@ -1,11 +1,32 @@
 package com.clinic.drug.controller;
 
-import com.clinic.AbstractController;
+import com.clinic.AbstractCrudController;
 import com.clinic.drug.domain.Medicine;
 import com.clinic.drug.repository.MedicineRepository;
 
-public class MedicineController extends AbstractController<Medicine, MedicineRepository>{
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
+public class MedicineController extends AbstractCrudController<Medicine, MedicineRepository>{
     public MedicineController() {
         super(Medicine.class, MedicineRepository.class);
+    }
+
+    @Override
+    protected void setFormGrid(GridPane formGrid, int action) {
+        Medicine entity = action == AbstractCrudController.CREATE_ACTION 
+            ? new Medicine() 
+            : entityTable.getSelectionModel().getSelectedItem();
+
+        TextField genericNameField = new TextField();
+        formGrid.addRow(1, new Label("Generic Name: "), genericNameField);
+        genericNameField.textProperty().bindBidirectional(entity.genericNameProperty());
+
+        TextField brandNameField = new TextField();
+        formGrid.addRow(2, new Label("Brand Name: "), brandNameField);
+        brandNameField.textProperty().bindBidirectional(entity.brandNameProperty());
+
+        formGrid.add(generateSubmitButton("Submit", entity, action), 1, 3);
     }
 }
