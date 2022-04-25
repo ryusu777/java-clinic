426ac23a90317536565fc8d61999c25c2a8ee513
Gaigeee <90014440+Gaigeee@users.noreply.github.com>
2022-04-25 10:05:45 +0700

    Penambahan CheckupCategory
    Penambahan CheckupCategory di domain

---
+++ src/main/java/com/clinic/drug/domain/CheckUpCategory.java
@@ -0,0 +1,10 @@
+package com.clinic.drug.domain;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.clinic.abstracts.AbstractEntity;
+
+public class CheckUpCategory extends AbstractEntity {
+    
+}
