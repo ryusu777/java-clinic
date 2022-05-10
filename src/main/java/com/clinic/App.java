c7932f31e0f48323cbbd255e5862342b22741260
Jose Ryu Leonesta <ryunamikaze777@gmail.com>
2022-05-10 22:15:19 +0700

    App.java for drug development.

--- src/main/java/com/clinic/App.java
+++ src/main/java/com/clinic/App.java
@@ -1,22 +1,52 @@
 package com.clinic;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Set;
+
+import com.clinic.abstracts.AbstractCrudController;
+import com.clinic.factories.CrudControllerFactory;
+
+import org.reflections.Reflections;
+
+import io.github.palexdev.materialfx.controls.MFXRectangleToggleNode;
+import io.github.palexdev.materialfx.controls.MFXScrollPane;
+import io.github.palexdev.materialfx.utils.ToggleButtonsUtil;
+
 
 /**
  * JavaFX App
  */
 public class App extends Application {
+    private VBox menus = new VBox();
+    private MFXScrollPane navbarPane = new MFXScrollPane(menus);
+    private BorderPane layout = new BorderPane();
+    private ToggleGroup toggleGroup;
+
     @Override
     public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("drug/views/medicine.fxml"));
-        stage.setTitle("Sample Controller");
-        stage.setScene(new Scene(root));
+        menus.setPrefWidth(300);
+        menus.setSpacing(10);
+        navbarPane.setPadding(new Insets(10));
+        navbarPane.setPrefWidth(340);
+        layout.setLeft(navbarPane);
+        layout.setPrefHeight(600);
+        layout.setPrefWidth(1000);
+        layout.setStyle("-fx-background-color: #ffffff");
+		this.toggleGroup = new ToggleGroup();
+		ToggleButtonsUtil.addAlwaysOneSelectedSupport(toggleGroup);
+        setCrudMenuButton("com.clinic.drug.controller");
+        stage.setScene(new Scene(layout));
         stage.show();
     }
 
@@ -24,4 +54,35 @@ public class App extends Application {
         ClinicConnection.connect();
         launch();
     }
+
+    public void setCrudMenuButton(String packageName) {
+        for (Class<? extends AbstractCrudController> controllerClass : findAllClassesInPackage(
+                packageName)) {
+            ToggleButton controllerButton = createToggle(controllerClass.getSimpleName());
+            controllerButton.setOnAction((event) -> {
+                AbstractCrudController controller = CrudControllerFactory.getController(controllerClass);
+                controller.fetchEntitiesToTable();
+                Parent controllerScene = controller.getMainScene().getRoot();
+                controllerScene.setStyle("-fx-background-color: #ffffff");
+                layout.setCenter(controllerScene);
+                controllerButton.setSelected(true);
+            });
+            menus.getChildren().add(controllerButton);
+        }
+    }
+
+    private Set<Class<? extends AbstractCrudController>> findAllClassesInPackage(String packageName) {
+        return new Reflections(packageName).getSubTypesOf(AbstractCrudController.class);
+    }
+
+    private ToggleButton createToggle(String text) {
+		MFXRectangleToggleNode toggleNode = new MFXRectangleToggleNode(text);
+        toggleNode.setMinHeight(50);
+		toggleNode.setAlignment(Pos.CENTER_LEFT);
+		toggleNode.setMaxWidth(Double.MAX_VALUE);
+		toggleNode.setToggleGroup(toggleGroup);
+
+		return toggleNode;
+	}
+        
 }
